f080426709de04c4fefd83413e97a4803f898299
Vitor Kasai <vitorkasai@estudante.ufscar.br>
2025-05-06 10:14:09 -0300

    feat: inicializando projeto spring

---
+++ leilao-veiculos/src/test/java/com/ufscar/dc/pooa/leilao/veiculos/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufscar.dc.pooa.leilao.veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
