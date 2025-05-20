96996b32a595c0e6ab1ab531492b5e08ef8580be
Vitor Kasai <vitorkasai@estudante.ufscar.br>
2025-05-20 14:46:08 -0300

    fix annotation

--- leilao-veiculos/src/main/java/com/ufscar/dc/pooa/leilao/veiculos/service/CompradorService.java
+++ leilao-veiculos/src/main/java/com/ufscar/dc/pooa/leilao/veiculos/service/CompradorService.java
@@ -1,9 +1,7 @@
 package com.ufscar.dc.pooa.leilao.veiculos.service;
 
 import com.ufscar.dc.pooa.leilao.veiculos.dto.CompradorDTO;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface CompradorService {
     void create(CompradorDTO dto);
 }
