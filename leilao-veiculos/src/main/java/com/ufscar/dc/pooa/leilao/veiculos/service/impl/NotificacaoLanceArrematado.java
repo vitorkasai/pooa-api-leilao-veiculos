fbdf423d9715007a7ad38680387b3bfab40fc149
Vitor Kasai <vitorkasai@estudante.ufscar.br>
2025-06-04 09:09:04 -0300

    feat: notificacao strategy impl

---
+++ leilao-veiculos/src/main/java/com/ufscar/dc/pooa/leilao/veiculos/service/impl/NotificacaoLanceArrematado.java
@@ -0,0 +1,16 @@
+package com.ufscar.dc.pooa.leilao.veiculos.service.impl;
+
+import com.ufscar.dc.pooa.leilao.veiculos.service.NotificacaoService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class NotificacaoLanceArrematado implements NotificacaoService {
+
+	@Override
+	public void createNotificacao() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ leilao-veiculos/src/main/java/com/ufscar/dc/pooa/leilao/veiculos/service/impl/NotificacaoLanceRecebido.java
@@ -0,0 +1,13 @@
+package com.ufscar.dc.pooa.leilao.veiculos.service.impl;
+
+import com.ufscar.dc.pooa.leilao.veiculos.service.NotificacaoService;
+
+public class NotificacaoLanceRecebido implements NotificacaoService {
+
+	@Override
+	public void createNotificacao() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
